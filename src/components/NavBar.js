b8fb059a33593f3635f8420769bfd227c8846ce0
Zach Johnson <zachjjohns@gmail.com>
2021-06-22 13:14:20 -0500

    Add first style to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,19 +4,33 @@ import { NavLink } from 'react-router-dom';
 
 export default function NavBar() {
   return (
-    <header>
-      <div>
-        <nav>
-          <NavLink to='/' exact>
+    <header className="bg-red-600">
+      <div className="container mx-auto flex justify-between">
+        <nav className="flex">
+          <NavLink 
+            to='/' 
+            exact
+            activeClassName="text-white"
+            className="inflex-flex items-center py-6 px-3 mr-4 text-red-100 hover:text-green-800 text-4xl font-bold cursive tracking-widest"
+          >
             Zach Johnson
           </NavLink>
-          <NavLink to='/post'>
+          <NavLink 
+            to='/post'
+            className="inflex-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+          >
             Blog Posts
           </NavLink>
-          <NavLink to='/project'>
+          <NavLink 
+            to='/project'
+            className="inflex-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+          >
             Projects
           </NavLink>
-          <NavLink to='/about'>
+          <NavLink 
+            to='/about'
+            className="inflex-flex items-center py-3 px-3 my-6 rounded text-red-200 hover:text-green-800"
+          >
             About Me!
           </NavLink>
         </nav>
