952ec663f46dc97a5ad6f6bdbefd39fc5afd36f8
Zach Johnson <zachjjohns@gmail.com>
2021-06-22 13:07:08 -0500

    Build NavBar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
 
 export default function NavBar() {
-  return <h1>NavBar!</h1>
+  return (
+    <header>
+      <div>
+        <nav>
+          <NavLink to='/' exact>
+            Zach Johnson
+          </NavLink>
+          <NavLink to='/post'>
+            Blog Posts
+          </NavLink>
+          <NavLink to='/project'>
+            Projects
+          </NavLink>
+          <NavLink to='/about'>
+            About Me!
+          </NavLink>
+        </nav>
+      </div>
+    </header>
+  )
 }
