5268d657c6580132fc87f638de858fc174c5bc58
Zach Johnson <zachjjohns@gmail.com>
2021-07-03 14:51:10 -0500

    Add background image and apply styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import image from "src/spacemountain.jpg";
 
 export default function Home() {
-  return <h1>Home page!</h1>
+  return (
+    <main>
+      <img 
+        src={image} 
+        alt="Mountain scene with starry sky" 
+        classNames="absolute object-cover w-full h-full"
+      />
+      <section>
+        <h1>Hey there! I'm Zach.</h1>
+      </section>
+    </main>
+  )
 }
