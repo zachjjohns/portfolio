4e2f0ea0891b968c468d498a5c5886937e326632
Zach Johnson <zachjjohns@gmail.com>
2021-07-03 14:37:29 -0500

    Adjust styling for SocialIcon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -39,7 +39,7 @@ export default function NavBar() {
         </nav>
         <div className="inline-flex py-3 px-3 my-6">
           <SocialIcon url="https://www.linkedin.com/in/zachjjohns/" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35}}/>
-          <SocialIcon url="" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35}}/>
+          <SocialIcon url="https://github.com/zachjjohns" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35}}/>
         </div>
       </div>
     </header>
