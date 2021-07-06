9545efca79613f6a6fe94e46a1d423ef7184b472
Zach Johnson <zachjjohns@gmail.com>
2021-07-06 10:48:04 -0500

    Build out GROQ queries

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,5 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import sanityClient from '../client.js';
 
 export default function Post() {
-  return <h1>Post page!</h1>
+  const [postData, setPost] = useState(null)
+
+  useEffect(() => {
+    sanityClient
+      .fetch(`*[_type == "post"]{
+        title,
+        slug,
+        mainImage{
+          asset->{
+            _id,
+            url
+          },
+          alt
+        }
+      }`)
+      .then((data) => setPost(data))
+      .catch(console.error);
+  }, []);
+  
+  return 
 }
