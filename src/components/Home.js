c1a2b15354360e4f584f329fa72700ca95edf9f8
Zach Johnson <zachjjohns@gmail.com>
2021-07-06 10:41:24 -0500

    Alter home message font size and finalize other CSS

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import image from "src/spacemountain.jpg";
+import image from "../../src/spacemountain.jpg";
 
 export default function Home() {
   return (
@@ -7,10 +7,13 @@ export default function Home() {
       <img 
         src={image} 
         alt="Mountain scene with starry sky" 
-        classNames="absolute object-cover w-full h-full"
+        className="absolute object-cover w-full h-full"
       />
-      <section>
-        <h1>Hey there! I'm Zach.</h1>
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h1 
+          className="text-6xl text-blue-100 font-bold ubuntu leading-none lg:leading-snug home-name">
+          Hello World. I'm Zach.
+        </h1>
       </section>
     </main>
   )
