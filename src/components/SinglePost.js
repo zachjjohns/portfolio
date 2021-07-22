33cad51c2e141e23589922a1cdb1ba566cfe46f3
Zach Johnson <zachjjohns@gmail.com>
2021-07-22 18:11:29 -0500

    Add imports to SinglePost

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import sanityClient from "../client.js";
 
 export default function SinglePost() {
+  const [SinglePost, setSinglePost] = useState(null);
+  const { slug } = useParams();
+  
   return <h1>SinglePost page!</h1>
 }
