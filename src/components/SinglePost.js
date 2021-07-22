cc4eb6a8505ada2dc610179b321d607ae88af546
Zach Johnson <zachjjohns@gmail.com>
2021-07-22 18:26:22 -0500

    Fill in first half of classNames

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -5,7 +5,7 @@ import imageUrlBuilder from "@sanity/image-url";
 
 const builder = imageUrlBuilder(sanityClient);
 function urlFor(source) {
-  return builder.image(source)
+  return builder.image(source);
 }
 
 export default function SinglePost() {
@@ -13,7 +13,9 @@ export default function SinglePost() {
   const { slug } = useParams();
 
   useEffect(() => {
-    sanityClient.fetch(`*[slug.current == "${slug}"]{
+    sanityClient
+      .fetch(
+        `*[slug.current == "${slug}"]{
       title,
       _id,
       slug,
@@ -26,26 +28,37 @@ export default function SinglePost() {
       body,
       "name": author->name,
       "authorImage": author->image
-    }`).then((data) => setSinglePost(data[0]))
-    .catch(console.error);
+    }`
+      )
+      .then((data) => setSinglePost(data[0]))
+      .catch(console.error);
   }, [slug]);
 
   if (!singlePost) return <div>Loading...</div>;
 
   return (
-    <main>
-      <article>
-        <header>
-          <div>
-            <div>
-              <h1></h1>
-              <div><img /></div>
-              <p></p>
+    <main className="bg-gray-200 min-h-screen p-12">
+      <article className="container shadow-lg mx-auto bg-green-100 rounded-lg">
+        <header className="relative">
+          <div className="absolute h-full w-full flex items-center justify-center p-8">
+            <div className="bg-white bg-opacity-75 rounded p-12">
+              <h1 className="cursive text-3xl lg:text-6xl mb-4">
+                {singlePost.title}
+              </h1>
+              <div className="flex justify-center text-gray-800">
+                <img
+                  src={urlFor(singlePost.authorImage).url()}
+                  alt={singlePost.name}
+                  className="w-10 h-10 rounded-full"
+                />
+                <p></p>
+              </div>
             </div>
           </div>
           <img />
         </header>
+        <div>BLOCK CONTENT</div>
       </article>
     </main>
-  )
+  );
 }
