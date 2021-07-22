8eca44d0127cc0d4349d947ac1d880ee7ff6f23c
Zach Johnson <zachjjohns@gmail.com>
2021-07-22 13:00:09 -0500

    Fix sizing for pointer span

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import project from "../../portfolio/schemas/project.js";
 import sanityClient from "../client.js";
 
 export default function Project() {
@@ -13,7 +12,7 @@ export default function Project() {
       date,
       place,
       description,
-      project,
+      projectType,
       link,
       tags
     }`
@@ -30,45 +29,49 @@ export default function Project() {
           Welcome to my projects page!
         </h2>
         <section className="grid grid-cols-2 gap-8">
-          <article className="relative rounded-lg shadow-xl bg-white p-16">
-            <h3 className="text-gray-800 text-3xl font-bold mb-2 hover:text-red-700">
-              <a
-                href={project.link}
-                alt={project.title}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {project.title}
-              </a>
-            </h3>
-            <div className="text-gray-500 text-xs space-x-4">
-              <span>
-                <strong className="font-bold">Finished on </strong>:{" "}
-                {new Date(project.date).toLocaleDateString()}
-              </span>
-              <span>
-                <strong className="font-bold">Company</strong>: {project.place}
-              </span>
-              <span>
-                <strong className="font-bold">Type</strong>:{" "}
-                {project.projectType}
-              </span>
-              <p className="my-6 text-lg text-gray-700 leading-relaxed">
-                {project.description}
-              </p>
-              <a
-                href={project.link}
-                rel="noopener noreferrer"
-                target="_blank"
-                className="text-red-500 font-bold hover:underline hover:text-red-400"
-              >
-                View the Project{" "}
-                <span role="img" aria-label="right pointer">
-                  👉
-                </span>
-              </a>
-            </div>
-          </article>
+          {projectData &&
+            projectData.map((project, index) => (
+              <article className="relative rounded-lg shadow-xl bg-white p-16">
+                <h3 className="text-gray-800 text-3xl font-bold mb-2 hover:text-red-700">
+                  <a
+                    href={project.link}
+                    alt={project.title}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {project.title}
+                  </a>
+                </h3>
+                <div className="text-gray-500 text-xs space-x-4">
+                  <span>
+                    <strong className="font-bold">Finished on </strong>:{" "}
+                    {new Date(project.date).toLocaleDateString()}
+                  </span>
+                  <span>
+                    <strong className="font-bold">Company</strong>:{" "}
+                    {project.place}
+                  </span>
+                  <span>
+                    <strong className="font-bold">Type</strong>:{" "}
+                    {project.projectType}
+                  </span>
+                  <p className="my-6 text-lg text-gray-700 leading-relaxed">
+                    {project.description}
+                  </p>
+                  <a
+                    href={project.link}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    className="text-red-500 font-bold hover:underline hover:text-red-400 text-xl"
+                  >
+                    View the Project{" "}
+                    <span role="img" aria-label="right pointer">
+                      👉
+                    </span>
+                  </a>
+                </div>
+              </article>
+            ))}
         </section>
       </section>
     </main>
