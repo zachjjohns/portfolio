78763dd111bf7ff377d0665a2380b91f06a76eec
Zach Johnson <zachjjohns@gmail.com>
2021-07-22 12:33:23 -0500

    Finish first half of classNames

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import project from "../../portfolio/schemas/project.js";
 import sanityClient from "../client.js";
 
 export default function Project() {
@@ -22,13 +23,20 @@ export default function Project() {
   }, []);
 
   return (
-    <main>
-      <section>
-        <h1></h1>
-        <h2></h2>
-        <section>
-          <article>
-            <h3></h3>
+    <main className="bg-green-100 min-h-screen p-12">
+      <section className="container mx-auto">
+        <h1 className="text-5xl flex justify-center cursive">My Projects</h1>
+        <h2 className="text-lg text-gray-600 flex justify-center mb-12">Welcome to my projects page!</h2>
+        <section className="grid grid-cols-2 gap-8">
+          <article className="relative rounded-lg shadow-xl bg-white p-16">
+            <h3 className="text-gray-800 text-3xl font-bold mb-2 hover:text-red-700">
+              <a
+                href={project.link}
+                alt={project.title}
+                target="_blank"
+                rel="noopener noreferrer"
+              >{project.title}</a>
+            </h3>
             <div>
               <span></span>
               <span></span>
