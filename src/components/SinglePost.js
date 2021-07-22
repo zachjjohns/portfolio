54cf754906e3640a5b4e6ca9af40344b4cd95bd6
Zach Johnson <zachjjohns@gmail.com>
2021-07-22 18:14:43 -0500

    add builder + urlFor function

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -1,10 +1,16 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import sanityClient from "../client.js";
+import imageUrlBuilder from "@sanity/image-url";
+
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source) {
+  return builder.image(source)
+}
 
 export default function SinglePost() {
   const [SinglePost, setSinglePost] = useState(null);
   const { slug } = useParams();
-  
+
   return <h1>SinglePost page!</h1>
 }
