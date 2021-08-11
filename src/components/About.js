440239ce52f17dd960f761d4f0a0003f776632bb
Zach Johnson <zachjjohns@gmail.com>
2021-08-11 15:03:47 -0500

    build sanity query for About

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import sanityClient from "../client";
 
 export default function About() {
-  return <h1>About page!</h1>
+  const [author, setAuthor] = useState(null);
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "author"]{
+      name,
+      bio,
+      "authorImage": image.aseet->url
+    }`
+      )
+      .then((data) => setAuthor(data[0]))
+      .catch(console.error);
+  });
+
+  if (!author) return <div>Loading...</div>
+
+  return <h1>About page!</h1>;
 }
