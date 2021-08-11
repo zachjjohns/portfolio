df741041bbc43e0db4618acf63a0013288468afd
Zach Johnson <zachjjohns@gmail.com>
2021-08-11 15:48:27 -0500

    Insert tailwind CSS, add imports for imageUrlBuilder/BlockContet

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import sanityClient from "../client";
 import dravania from "../dravania.jpeg";
 import imageUrlBuilder from "./@sanity/image-url";
+import BlockContent from "@sanity/block-content-to-react";
 
 const builder = imageUrlBuilder(sanityClient);
 function urlFor(source) {
@@ -32,10 +33,13 @@ export default function About() {
       <div className="p-10 lg:pt-48 container mx-auto relative">
         <section className="bg-green-800 rounded-lg">
           <img src={urlFor(author.authorImage).url()} className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8" alt={author.name}/>
-          <div>
-            <h1></h1>
-            <div>
-              BLOCK CONTENT
+          <div className="text-lg flex flex-col justify-center">
+            <h1 className="cursive text-6xl text-green-300 mb-4">
+              Hey there. I'm{" "}
+              <span className="text-green-100">{author.name}</span>
+            </h1>
+            <div className="prose lg:prose-xl text-white">
+              <BlockContent blocks={author.bio} projectId="w5oew6cm" dataset="production"/>
             </div>
           </div>
         </section>
