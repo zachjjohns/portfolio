78e96a78d4793092ce013d8b0dc285401104b6b5
Zach Johnson <zachjjohns@gmail.com>
2021-08-11 16:07:07 -0500

    Fix CSS for section

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import sanityClient from "../client";
 import dravania from "../dravania.jpeg";
-import imageUrlBuilder from "./@sanity/image-url";
+import imageUrlBuilder from "@sanity/image-url";
 import BlockContent from "@sanity/block-content-to-react";
 
 const builder = imageUrlBuilder(sanityClient);
@@ -18,33 +18,44 @@ export default function About() {
         `*[_type == "author"]{
       name,
       bio,
-      "authorImage": image.aseet->url
+      "authorImage": image.asset->url
     }`
       )
       .then((data) => setAuthor(data[0]))
       .catch(console.error);
   });
 
-  if (!author) return <div>Loading...</div>
+  if (!author) return <div>Loading...</div>;
 
   return (
     <main className="relative">
-      <img src={dravania} alt="Artwork of abstract city in sky from Final Fantasy 14" className="absolute w-full" />
+      <img
+        src={dravania}
+        alt="Artwork of abstract city in sky from Final Fantasy 14"
+        className="absolute w-full"
+      />
       <div className="p-10 lg:pt-48 container mx-auto relative">
-        <section className="bg-green-800 rounded-lg">
-          <img src={urlFor(author.authorImage).url()} className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8" alt={author.name}/>
+        <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-10">
+          <img
+            src={urlFor(author.authorImage).url()}
+            className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8"
+            alt={author.name}
+          />
           <div className="text-lg flex flex-col justify-center">
-            <h1 className="cursive text-6xl text-green-300 mb-4">
+            <h1 className="cursive text-5xl text-green-300 mb-4">
               Hey there. I'm{" "}
               <span className="text-green-100">{author.name}</span>
             </h1>
             <div className="prose lg:prose-xl text-white">
-              <BlockContent blocks={author.bio} projectId="w5oew6cm" dataset="production"/>
+              <BlockContent
+                blocks={author.bio}
+                projectId="w5oew6cm"
+                dataset="production"
+              />
             </div>
           </div>
         </section>
       </div>
     </main>
-
   );
 }
