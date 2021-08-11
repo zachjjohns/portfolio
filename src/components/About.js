b371ab92781d509b0604c4132c8f257d4f5cc0c0
Zach Johnson <zachjjohns@gmail.com>
2021-08-11 15:40:08 -0500

    Create urlFor method, use it for img src

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from "react";
 import sanityClient from "../client";
 import dravania from "../dravania.jpeg";
+import imageUrlBuilder from "./@sanity/image-url";
+
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source) {
+  return builder.image(source);
+}
 
 export default function About() {
   const [author, setAuthor] = useState(null);
@@ -21,11 +27,11 @@ export default function About() {
   if (!author) return <div>Loading...</div>
 
   return (
-    <main>
-      <img />
-      <div>
-        <section>
-          <img />
+    <main className="relative">
+      <img src={dravania} alt="Artwork of abstract city in sky from Final Fantasy 14" className="absolute w-full" />
+      <div className="p-10 lg:pt-48 container mx-auto relative">
+        <section className="bg-green-800 rounded-lg">
+          <img src={urlFor(author.authorImage).url()} className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8" alt={author.name}/>
           <div>
             <h1></h1>
             <div>
