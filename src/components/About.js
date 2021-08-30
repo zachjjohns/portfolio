dfd703fe508f43e5165dcee8c85eee2688561e66
Zach Johnson <zachjjohns@gmail.com>
2021-08-30 15:45:26 -0500

    Change greens to blues for aboutme page

--- src/components/About.js
+++ src/components/About.js
@@ -35,16 +35,16 @@ export default function About() {
         className="absolute w-full"
       />
       <div className="p-10 lg:pt-48 container mx-auto relative">
-        <section className="bg-green-800 rounded-lg shadow-2xl lg:flex p-10">
+        <section className="bg-blue-800 rounded-lg shadow-2xl lg:flex p-10">
           <img
             src={urlFor(author.authorImage).url()}
             className="rounded w-32 h-32 lg:w-64 lg:h-64 mr-8"
             alt={author.name}
           />
           <div className="text-lg flex flex-col justify-center">
-            <h1 className="cursive text-5xl text-green-300 mb-4">
+            <h1 className="cursive text-5xl text-blue-400 mb-4">
               Hey there. I'm{" "}
-              <span className="text-green-100">{author.name}</span>
+              <span className="text-blue-200">{author.name}:</span>
             </h1>
             <div className="prose lg:prose-xl text-white">
               <BlockContent
